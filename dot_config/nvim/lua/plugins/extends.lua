b6e0463c9eeb9a5ac1c179f9acd771bf60e14cbb
chg1f <chongiofai@gmail.com>
2024-10-08 14:33:28 +0800

    Update .config/Brewfile Update .config/nvim/lua/plugins/extends.lua Update .config/zsh/.zimrc Update .config/zsh/.zshrc

--- dot_config/nvim/lua/plugins/extends.lua
+++ dot_config/nvim/lua/plugins/extends.lua
@@ -7,15 +7,15 @@ plugins = vim.list_extend(plugins, {
     config = function()
       -- we disable most of these features because treesitter and nvim-lsp
       -- take care of it
-      vim.g["go_gopls_enabled"] = 0
-      vim.g["go_code_completion_enabled"] = 0
-      vim.g["go_fmt_autosave"] = 0
-      vim.g["go_imports_autosave"] = 0
-      vim.g["go_mod_fmt_autosave"] = 0
-      vim.g["go_doc_keywordprg_enabled"] = 0
-      vim.g["go_def_mapping_enabled"] = 0
-      vim.g["go_textobj_enabled"] = 0
-      vim.g["go_list_type"] = "quickfix"
+      vim.g.go_gopls_enabled = 0
+      vim.g.go_code_completion_enabled = 0
+      vim.g.go_fmt_autosave = 0
+      vim.g.go_imports_autosave = 0
+      vim.g.go_mod_fmt_autosave = 0
+      vim.g.go_doc_keywordprg_enabled = 0
+      vim.g.go_def_mapping_enabled = 0
+      vim.g.go_textobj_enabled = 0
+      vim.g.go_list_type = "quickfix"
     end,
   },
   {
