32e596cdd33435b460a9ca810da1aba2e88b6f6b
chg1f <chongiofai@gmail.com>
2024-10-09 17:32:06 +0800

    Update .config/Brewfile Update .config/wezterm/wezterm.lua Update .zshenv

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,26 +1,50 @@
 local wezterm = require("wezterm")
-return {
+local config = {
+  automatically_reload_config = true,
+  exit_behavior = "CloseOnCleanExit",
+  exit_behavior_messaging = "Verbose",
+
+  animation_fps = 60,
+  max_fps = 60,
+  front_end = "WebGpu",
+  webgpu_power_preference = "HighPerformance",
+
+  color_scheme = "Tokyo Night",
   font = wezterm.font_with_fallback({
     { family = "JetBrains Mono NL" },
     { family = "Noto Color Emoji" },
   }),
-  color_scheme = "Tokyo Night",
-  window_close_confirmation = "AlwaysPrompt",
-  exit_behavior = "CloseOnCleanExit",
+  font_size = 12.0,
+
+  window_close_confirmation = "NeverPrompt",
+  window_padding = { left = 0.5, right = 0.5, top = 0.5, bottom = 0.5 },
+
+  enable_tab_bar = true,
   hide_tab_bar_if_only_one_tab = true,
+  show_tab_index_in_tab_bar = true,
+  show_close_tab_button_in_tabs = true,
+  show_new_tab_button_in_tab_bar = true,
+  switch_to_last_active_tab_when_closing_tab = true,
+
   disable_default_key_bindings = true,
   keys = {
     { key = "c", mods = "SUPER", action = wezterm.action.CopyTo("Clipboard") },
     { key = "v", mods = "SUPER", action = wezterm.action.PasteFrom("Clipboard") },
+
     { key = "+", mods = "SUPER", action = wezterm.action.IncreaseFontSize },
     { key = "-", mods = "SUPER", action = wezterm.action.DecreaseFontSize },
     { key = "0", mods = "SUPER", action = wezterm.action.ResetFontSize },
+
+    { key = "t", mods = "SUPER", action = wezterm.action.SpawnTab("DefaultDomain") },
+    { key = "w", mods = "SUPER", action = wezterm.action.CloseCurrentTab({ confirm = false }) },
+    { key = "Tab", action = wezterm.action.ActivateTabRelative(1) },
   },
-  mouse_bindings = {
-    {
-      event = { Up = { streak = 1, button = "Left" } },
-      mods = "CTRL",
-      action = wezterm.action.OpenLinkAtMouseCursor,
-    },
-  },
+  -- mouse_bindings = {
+  --   {
+  --     event = { Up = { streak = 1, button = "Left" } },
+  --     mods = "CTRL",
+  --     action = wezterm.action.OpenLinkAtMouseCursor,
+  --   },
+  -- },
 }
+return config
