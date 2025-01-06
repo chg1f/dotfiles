c2f18ad1500fc43c7e4d7b64e0eb1760e6b44799
chg1f <chongiofai@gmail.com>
2025-01-06 15:53:45 +0800

    Update .config/homebrew/Brewfile Update .config/wezterm/wezterm.lua Update .profile

--- dot_config/exact_wezterm/wezterm.lua
+++ dot_config/exact_wezterm/wezterm.lua
@@ -4,33 +4,25 @@ local config = {
 	exit_behavior = "CloseOnCleanExit",
 	exit_behavior_messaging = "Verbose",
 
-	term = "xterm-256color",
-	-- term = "wezterm",
+	window_close_confirmation = "NeverPrompt",
+	window_padding = { left = 0.5, right = 0.5, top = 0.5, bottom = 0.5 },
+
+	enable_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
 
 	animation_fps = 60,
 	max_fps = 60,
 	front_end = "WebGpu",
 	webgpu_power_preference = "HighPerformance",
 
-	-- color_scheme = "Tokyo Night",
 	color_scheme = "GruvboxDark",
+	font_size = 12,
 	font = wezterm.font_with_fallback({
 		{ family = "JetBrains Mono NL" },
 		{ family = "Noto Sans CJK TC" },
 		{ family = "Noto Sans CJK SC" },
 		{ family = "Noto Emoji" },
 	}),
-	font_size = 12,
-
-	window_close_confirmation = "NeverPrompt",
-	window_padding = { left = 0.5, right = 0.5, top = 0.5, bottom = 0.5 },
-
-	enable_tab_bar = true,
-	hide_tab_bar_if_only_one_tab = true,
-	show_tab_index_in_tab_bar = true,
-	show_close_tab_button_in_tabs = true,
-	show_new_tab_button_in_tab_bar = true,
-	switch_to_last_active_tab_when_closing_tab = true,
 
 	disable_default_key_bindings = true,
 	keys = {
