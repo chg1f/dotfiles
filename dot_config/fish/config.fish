fd3845f0e4b94aba79cfb512d751f9b4039536c5
chg1f <chongiofai@gmail.com>
2025-01-20 11:28:10 +0800

    Update .config/homebrew/Brewfile Update .config/fish/config.fish Update .config/fish/fish_plugins Update .config/git/config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -19,4 +19,57 @@ set -x TMUX_PLUGIN_MANAGER_PATH $XDG_DATA_HOME/tmux/plugins
 
 if status is-interactive
     fish_vi_key_bindings
+
+    set -U fish_greeting
+
+    set -U tide_prompt_add_newline_before true
+    set -U tide_left_prompt_items status pwd git newline character vi_mode
+    set -U tide_status_icon
+    set -U tide_status_icon_failure
+    set -U tide_status_color green
+    set -U tide_status_color_failure red
+    set -U tide_pwd_markers .git package.json pyproject.toml go.work go.mod Cargo.toml build.zig
+    set -U tide_git_icon
+    set -U tide_git_color_branch cyan
+    set -U tide_git_color_conflicted red
+    set -U tide_git_color_dirty yellow
+    set -U tide_git_color_operation green
+    set -U tide_git_color_staged brgreen
+    set -U tide_git_color_stash brblack
+    set -U tide_git_color_untracked brred
+    set -U tide_git_color_upstream brcyan
+    set -U tide_character_icon \#
+    set -U tide_character_vi_icon_default
+    set -U tide_character_vi_icon_replace
+    set -U tide_character_vi_icon_visual
+    set -U tide_character_color brblack
+    set -U tide_character_color_failure brblack
+    set -U tide_vi_mode_icon_insert
+    set -U tide_vi_mode_color_insert normal
+    set -U tide_vi_mode_icon_default \[c\]
+    set -U tide_vi_mode_color_default yellow
+    set -U tide_vi_mode_icon_replace \[r\]
+    set -U tide_vi_mode_color_replace red
+    set -U tide_vi_mode_icon_visual \[v\]
+    set -U tide_vi_mode_color_visual brred
+    set -U tide_right_prompt_items jobs direnv python node go rustc zig context
+    set -U tide_jobs_icon jobs
+    set -U tide_jobs_color yellow
+    set -U tide_direnv_icon direnv
+    set -U tide_direnv_color green
+    set -U tide_direnv_color_denied red
+    set -U tide_python_icon python
+    set -U tide_python_color brblack
+    set -U tide_node_icon node
+    set -U tide_node_color brblack
+    set -U tide_rustc_icon rustc
+    set -U tide_rustc_color brblack
+    set -U tide_go_icon go
+    set -U tide_go_color brblack
+    set -U tide_zig_icon zig
+    set -U tide_zig_color brblack
+    set -U tide_context_always_display true
+    set -U tide_context_color_default green
+    set -U tide_context_color_root red
+    set -U tide_context_color_ssh yellow
 end
