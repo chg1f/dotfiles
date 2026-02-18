619c4206f64d235a0a2d5c42402a1fd63bf7ca54
chg1f <chongiofai@gmail.com>
2026-02-18 22:33:54 +0800

    source d3a5d0a636982d24f104718de298b8a7 sync 1 files

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local config = {
+	adjust_window_size_when_changing_font_size = false,
 	initial_cols = 100,
 	initial_rows = 30,
 
@@ -18,12 +19,7 @@ local config = {
 	front_end = "WebGpu",
 	webgpu_power_preference = "HighPerformance",
 
-	-- color_scheme = "cyberdream",
-	-- color_scheme = "Night Owl (Gogh)",
 	color_scheme = "Tokyo Night Storm",
-	-- color_scheme = "One Dark Pro (Gogh)",
-	-- color_scheme = "Catppuccin Mocha",
-	-- color_scheme = "Kanagawa (Gogh)",
 	font_size = 12,
 	font = wezterm.font_with_fallback({
 		-- { family = "JetBrainsMono Nerd Font" },
