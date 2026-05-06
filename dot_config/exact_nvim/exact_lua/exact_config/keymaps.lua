7c20a57d81f8e4f7c9fd679107569d6fd39b9f4b
chg1f <chongiofai@gmail.com>
2026-05-06 09:53:59 +0800

    from c39ed2a790a7246dc974e4e9e4b1a3e1d8a51e5917aa8542bf892e097218d20f

--- dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "]g", function()
+	require("gitsigns").nav_hunk("next")
+end, { desc = "Next Git Hunk" })
+
+vim.keymap.set("n", "[g", function()
+	require("gitsigns").nav_hunk("prev")
+end, { desc = "Prev Git Hunk" })
