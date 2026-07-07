90e05d34bfa230daabdcca700eeda35bb8c0d305
chg1f <chongiofai@gmail.com>
2026-07-07 10:49:32 +0800

    from c39ed2a790a7246dc974e4e9e4b1a3e1d8a51e5917aa8542bf892e097218d20f

--- dot_config/exact_zsh/aliases.zsh
+++ dot_config/exact_zsh/aliases.zsh
@@ -1,16 +1,16 @@
 #!zsh
 
 alias less='less -R'
+alias path='print -l ${(s/:/)PATH} | nl'
 
+alias GNU='PATH="$(brew --prefix coreutils)/libexec/gnubin:$(brew --prefix findutils)/libexec/gnubin:$(brew --prefix diffutils)/libexec/gnubin:$PATH"'
+alias UUTILS='PATH="$(brew --prefix uutils-coreutils)/libexec/gnubin:$(brew --prefix uutils-findutils)/libexec/gnubin:$(brew --prefix uutils-diffutils)/libexec/gnubin:$PATH"'
 alias PX='HTTP_PROXY=http://127.0.0.1:1080 HTTPS_PROXY=http://127.0.0.1:1080 SOCKS_PROXY=socks5://127.0.0.1:1080 ALL_PROXY=socks5://127.0.0.1:1080 NO_PROXY=localhost,127.0.0.1,::1'
 alias -g Q="1>/dev/null 2>/dev/null"
 alias -g Q1="1>/dev/null"
 alias -g Q2="2>/dev/null"
 
 alias hurl='hurl --file-root . --cookie .cookie --variables-file .env --error-format long --continue-on-error'
-
-alias path='print -l ${(s/:/)PATH} | nl'
-
 # if command -v hurl &> /dev/null; then
 #   alias hurl="hurl --error-format=long"
 # fi
@@ -23,6 +23,7 @@ if command -v python3 &>/dev/null; then
   alias urldecode='python3 -c "import sys, urllib.parse; print(urllib.parse.unquote(sys.argv[1]))"'
   alias urlencode='python3 -c "import sys, urllib.parse; print(urllib.parse.quote(sys.argv[1]))"'
 fi
+
 if command -v eza &>/dev/null; then
   EZA_PARAMS=('--git' '--group' '--group-directories-first' '--time-style=+%y-%m-%d %H:%M:%S %a' '--color-scale=all' '--color=always')
   alias ls='eza $EZA_PARAMS'
@@ -33,6 +34,7 @@ if command -v eza &>/dev/null; then
   alias lx='eza $EZA_PARAMS --long --binary --header --links --inode --group --group-directories-first --modified --all --blocksize --classify=auto'
   alias lt='eza $EZA_PARAMS --tree'
 fi
+
 if command -v chezmoi 2>&1 >/dev/null; then
   alias cz='chezmoi --color=on'
   # alias cz-manages='chezmoi managed --tree --path-style=source-relative --no-pager'
