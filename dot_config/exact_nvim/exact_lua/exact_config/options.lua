e2ed35d837e5cab361017f3c710e8149d5b149b3
chg1f <chongiofai@gmail.com>
2026-08-26 16:23:18 +0800

    from c39ed2a790a7246dc974e4e9e4b1a3e1d8a51e5917aa8542bf892e097218d20f

--- dot_config/exact_nvim/exact_lua/exact_config/options.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/options.lua
@@ -28,6 +28,7 @@ vim.opt.cursorlineopt = "both"
 
 vim.g.bigfile_size = 500 * 1024 -- 500 KB
 
+vim.g.autoformat = true
 -- vim.opt.foldclose = ""
 -- vim.opt.viewoptions:remove("folds")
 -- vim.opt.sessionoptions:remove("folds")
